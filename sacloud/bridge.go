cb67effa35093abb52ce9ff57f563f9f2e061722
febc.yamamoto <yamamoto.febc@gmail.com>
2016-05-19 15:32:35 +0900

    Refactoring bridge

--- sacloud/bridge.go
+++ sacloud/bridge.go
@@ -6,13 +6,13 @@ type Bridge struct {
 	*Resource
 	Name        string `json:",omitempty"`
 	Description string `json:",omitempty"`
-	Info        struct {
+	Info        *struct {
 		Switches []Switch
 	}
 	ServiceClass string     `json:",omitempty"`
 	CreatedAt    *time.Time `json:",omitempty"`
 	Region       *Region    `json:",omitempty"`
-	SwitchInZone struct {
+	SwitchInZone *struct {
 		*Resource
 		Name           string `json:",omitempty"`
 		ServerCount    int    `json:",omitempty"`
