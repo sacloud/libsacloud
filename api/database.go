514392afdb77fc397266bc8ee0d5ffe568f35de1
febc.yamamoto <yamamoto.febc@gmail.com>
2016-09-22 09:22:53 +0900

    Fix monitoring functions

--- api/database.go
+++ api/database.go
@@ -261,18 +261,18 @@ func (api *DatabaseAPI) SleepWhileCopying(id int64, timeout time.Duration, maxRe
 	}
 }
 
-func (api *DatabaseAPI) MonitorCPU(id int64, nicIndex int, body *sacloud.ResourceMonitorRequest) (*sacloud.MonitorValues, error) {
+func (api *DatabaseAPI) MonitorCPU(id int64, body *sacloud.ResourceMonitorRequest) (*sacloud.MonitorValues, error) {
 	return api.baseAPI.applianceMonitorBy(id, "cpu", 0, body)
 }
-func (api *DatabaseAPI) MonitorDatabase(id int64, nicIndex int, body *sacloud.ResourceMonitorRequest) (*sacloud.MonitorValues, error) {
-	return api.baseAPI.applianceMonitorBy(id, "database", 0, nicIndex, body)
+func (api *DatabaseAPI) MonitorDatabase(id int64, body *sacloud.ResourceMonitorRequest) (*sacloud.MonitorValues, error) {
+	return api.baseAPI.applianceMonitorBy(id, "database", 0, body)
 }
-func (api *DatabaseAPI) MonitorInterface(id int64, nicIndex int, body *sacloud.ResourceMonitorRequest) (*sacloud.MonitorValues, error) {
-	return api.baseAPI.applianceMonitorBy(id, "interface", 0, nicIndex, body)
+func (api *DatabaseAPI) MonitorInterface(id int64, body *sacloud.ResourceMonitorRequest) (*sacloud.MonitorValues, error) {
+	return api.baseAPI.applianceMonitorBy(id, "interface", 0, body)
 }
-func (api *DatabaseAPI) MonitorSystemDisk(id int64, nicIndex int, body *sacloud.ResourceMonitorRequest) (*sacloud.MonitorValues, error) {
-	return api.baseAPI.applianceMonitorBy(id, "disk", 1, nicIndex, body)
+func (api *DatabaseAPI) MonitorSystemDisk(id int64, body *sacloud.ResourceMonitorRequest) (*sacloud.MonitorValues, error) {
+	return api.baseAPI.applianceMonitorBy(id, "disk", 1, body)
 }
-func (api *DatabaseAPI) MonitorBackupDisk(id int64, nicIndex int, body *sacloud.ResourceMonitorRequest) (*sacloud.MonitorValues, error) {
-	return api.baseAPI.applianceMonitorBy(id, "disk", 2, nicIndex, body)
+func (api *DatabaseAPI) MonitorBackupDisk(id int64, body *sacloud.ResourceMonitorRequest) (*sacloud.MonitorValues, error) {
+	return api.baseAPI.applianceMonitorBy(id, "disk", 2, body)
 }
