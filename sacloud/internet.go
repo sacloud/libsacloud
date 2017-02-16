e5d67173747da57c7065fc2b902e2ec660d065c0
febc.yamamoto <yamamoto.febc@gmail.com>
2017-02-16 09:38:06 +0900

    Add getter/setter to internet.go

--- sacloud/internet.go
+++ sacloud/internet.go
@@ -18,3 +18,33 @@ type Internet struct {
 	//TODO Zone(API側起因のデータ型不一致のため)
 	// ZoneType
 }
+
+// GetBandWidthMbps 帯域幅 取得
+func (i *Internet) GetBandWidthMbps() int {
+	return i.BandWidthMbps
+}
+
+// SetBandWidthMbps 帯域幅 設定
+func (i *Internet) SetBandWidthMbpx(v int) {
+	i.BandWidthMbps = v
+}
+
+// GetNetworkMaskLen ネットワークマスク長 取得
+func (i *Internet) GetNetworkMaskLen() int {
+	return i.NetworkMaskLen
+}
+
+// SetNetworkMaskLen ネットワークマスク長 設定
+func (i *Internet) SetNetworkMaskLen(v int) {
+	i.NetworkMaskLen = v
+}
+
+// AllowInternetBandWidth 設定可能な帯域幅の値リスト
+func AllowInternetBandWidth() []int {
+	return []int{100, 500, 1000, 1500, 2000, 2500, 3000}
+}
+
+// AllowInternetNetworkMaskLen 設定可能なネットワークマスク長の値リスト
+func AllowInternetNetworkMaskLen() []int {
+	return []int{26, 27, 28}
+}
