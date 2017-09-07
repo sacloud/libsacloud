a07d219980959fca3ca2e27b1d26c3ffa285cd52
febc.yamamoto <yamamoto.febc@gmail.com>
2017-09-07 14:34:16 +0900

    Add NFS Appliance

---
+++ api/nfs_test.go
@@ -0,0 +1,101 @@
+package api
+
+import (
+	"fmt"
+	"github.com/sacloud/libsacloud/sacloud"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+const testNFSName = "libsacloud_test_NFS"
+
+var createNFSValues = &sacloud.CreateNFSValue{
+	Plan:         sacloud.NFSPlan100G,
+	IPAddress:    "192.168.11.11",
+	MaskLen:      24,
+	DefaultRoute: "192.168.11.1",
+	Name:         "TestNFS",
+	Description:  "TestDescription",
+	Tags:         []string{"tag1", "tag2", "tag3"},
+}
+
+func TestNFSCRUD(t *testing.T) {
+	defer initNFS()()
+
+	api := client.NFS
+
+	//prerequired
+	sw := client.Switch.New()
+	sw.Name = testNFSName
+	sw, err := client.Switch.Create(sw)
+
+	assert.NoError(t, err)
+	assert.NotEmpty(t, sw)
+
+	//CREATE
+	createNFSValues.SwitchID = fmt.Sprintf("%d", sw.ID)
+	newItem := sacloud.NewNFS(createNFSValues)
+
+	item, err := api.Create(newItem)
+
+	assert.NoError(t, err)
+	assert.NotEmpty(t, item)
+
+	id := item.ID
+
+	err = api.SleepWhileCopying(id, client.DefaultTimeoutDuration, 30)
+	if !assert.NoError(t, err) {
+		return
+	}
+
+	err = api.SleepUntilUp(id, client.DefaultTimeoutDuration)
+	if !assert.NoError(t, err) {
+		return
+	}
+
+	//READ
+	item, err = api.Read(id)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, item)
+
+	//UPDATE
+	item.Description = "after"
+	item, err = api.Update(id, item)
+
+	assert.NoError(t, err)
+	assert.NotEqual(t, item.Description, "before")
+
+	_, err = api.Stop(id)
+	assert.NoError(t, err)
+
+	err = api.SleepUntilDown(id, client.DefaultTimeoutDuration)
+	if !assert.NoError(t, err) {
+		return
+	}
+
+	//Delete
+	_, err = api.Delete(id)
+	assert.NoError(t, err)
+
+	_, err = client.Switch.Delete(sw.ID)
+	assert.NoError(t, err)
+
+}
+
+func initNFS() func() {
+	cleanupNFS()
+	return cleanupNFS
+}
+
+func cleanupNFS() {
+	sw, _ := client.Switch.Reset().WithNameLike(testNFSName).Find()
+	for _, item := range sw.Switches {
+		client.Switch.Delete(item.ID)
+	}
+
+	items, _ := client.NFS.Reset().WithNameLike(testNFSName).Find()
+	for _, item := range items.NFS {
+		client.NFS.Delete(item.ID)
+	}
+
+}
---
+++ sacloud/nfs_test.go
@@ -0,0 +1,112 @@
+package sacloud
+
+import (
+	"encoding/json"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+var testNFSJSON = `
+{
+       "Availability": "available",
+        "Class": "nfs",
+        "CreatedAt": "2017-09-07T12:20:15+09:00",
+        "Description": "nfs-description",
+        "ID": 112901132074,
+        "Icon": {
+            "ID": 112901044094,
+            "Name": "myicon",
+            "Scope": "user",
+            "URL": "https://secure.sakura.ad.jp/cloud/zone/is1b/api/cloud/1.1/icon/112901044094.png"
+        },
+        "Instance": {
+            "Status": "up",
+            "StatusChangedAt": "2017-09-07T12:22:33+09:00"
+        },
+        "Interfaces": [
+            {
+                "HostName": null,
+                "IPAddress": null,
+                "Switch": {
+                    "ID": 112900591751,
+                    "Name": "vpc_router_for_vpn",
+                    "Scope": "user",
+                    "Subnet": null,
+                    "UserSubnet": {
+                        "DefaultRoute": "192.168.150.1",
+                        "NetworkMaskLen": 24
+                    }
+                },
+                "UserIPAddress": "192.168.150.101"
+            }
+        ],
+        "Name": "nfs-test",
+        "Plan": {
+            "ID": 100
+        },
+        "Remark": {
+            "Network": {
+                "DefaultRoute": "192.168.150.1",
+                "NetworkMaskLen": 24
+            },
+            "Plan": {
+                "ID": 100
+            },
+            "Servers": [
+                {
+                    "IPAddress": "192.168.150.101"
+                }
+            ],
+            "Switch": {
+                "ID": "112900591751"
+            },
+            "Zone": {
+                "ID": "31002"
+            }
+        },
+        "ServiceClass": "cloud/appliance/nfs/100gb",
+        "Settings": null,
+        "SettingsHash": null,
+        "Switch": {
+            "Availability": "available",
+            "ID": 112900591751,
+            "Internet": null,
+            "Name": "vpc_router_for_vpn",
+            "Scope": "user",
+            "Zone": {
+                "ID": 31002,
+                "Name": "is1b",
+                "Region": {
+                    "ID": 310,
+                    "Name": "\u77f3\u72e9"
+                }
+            }
+        },
+        "Tags": [
+            "tag1",
+            "tag2"
+        ]
+    }
+    	`
+
+func TestMarshalNFSJSON(t *testing.T) {
+	var nfs NFS
+	err := json.Unmarshal([]byte(testLoadBalancerJSON), &nfs)
+
+	assert.NoError(t, err)
+	assert.NotEmpty(t, nfs)
+
+	assert.NotEmpty(t, nfs.ID)
+	assert.NotEmpty(t, nfs.Remark)
+
+	assert.NotEmpty(t, nfs.Remark.Servers)
+	assert.NotEmpty(t, nfs.Remark.Network)
+	assert.NotEmpty(t, nfs.Remark.Switch)
+	//TODO Zone
+	//assert.NotEmpty(t, nfs.Remark.Zone)
+	//assert.NotEmpty(t, nfs.Remark.Plan)
+
+	assert.NotEmpty(t, nfs.Instance)
+	assert.NotEmpty(t, nfs.Interfaces)
+
+}
