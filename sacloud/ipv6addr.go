570ec167a1b1fb63c22edec2d07ae317f237329b
febc.yamamoto <yamamoto.febc@gmail.com>
2017-10-26 20:21:15 +0900

    Add IPv6Add functions

--- sacloud/ipv6addr.go
+++ sacloud/ipv6addr.go
@@ -9,6 +9,22 @@ type IPv6Addr struct {
 
 }
 
+// GetIPv6NetID IPv6アドレスが所属するIPv6NetのIDを取得
+func (a *IPv6Addr) GetIPv6NetID() int64 {
+	if a.IPv6Net != nil {
+		return a.IPv6Net.ID
+	}
+	return 0
+}
+
+// GetInternetID IPv6アドレスを所有するルータ+スイッチ(Internet)のIDを取得
+func (a *IPv6Addr) GetInternetID() int64 {
+	if a.IPv6Net != nil && a.IPv6Net.Switch != nil && a.IPv6Net.Switch.Internet != nil {
+		return a.IPv6Net.Switch.Internet.ID
+	}
+	return 0
+}
+
 // CreateNewIPv6Addr IPv6アドレス作成
 func CreateNewIPv6Addr() *IPv6Addr {
 	return &IPv6Addr{
