446df8e0b262daaa4cb5e82d7fae73a0dc5c54d4
febc.yamamoto <yamamoto.febc@gmail.com>
2017-10-26 12:37:47 +0900

    Fix IPv4/v6 model

--- sacloud/ipv6addr.go
+++ sacloud/ipv6addr.go
@@ -2,10 +2,10 @@ package sacloud
 
 // IPv6Addr IPアドレス(IPv6)
 type IPv6Addr struct {
-	HostName  string    `json:",omitempty"` // ホスト名
-	IPv6Addr  string    `json:",omitempty"` // IPv6アドレス
-	Interface *Internet `json:",omitempty"` // インターフェース
-	IPv6Net   *IPv6Net  `json:",omitempty"` // IPv6サブネット
+	HostName  string     `json:",omitempty"` // ホスト名
+	IPv6Addr  string     `json:",omitempty"` // IPv6アドレス
+	Interface *Interface `json:",omitempty"` // インターフェース
+	IPv6Net   *IPv6Net   `json:",omitempty"` // IPv6サブネット
 
 }
 
