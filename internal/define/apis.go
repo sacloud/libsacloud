e0f1649bdffe5f378ba095539bc6a5bd66d19205
febc.yamamoto <yamamoto.febc@gmail.com>
2019-05-17 20:25:10 +0900

    Add stubs

--- internal/define/apis.go
+++ internal/define/apis.go
@@ -4,6 +4,7 @@
 //go:generate go run ../tools/gen-api-envelope/main.go
 //go:generate go run ../tools/gen-api-op/main.go
 //go:generate go run ../tools/gen-api-tracer/main.go
+//go:generate go run ../tools/gen-api-stub/main.go
 package define
 
 import "github.com/sacloud/libsacloud-v2/internal/schema"
