8cb18d2593d80cd5d46a3f73537ef41e57dae917
febc.yamamoto <yamamoto.febc@gmail.com>
2019-07-25 16:21:44 +0900

    Server Util: FindPlan

---
+++ utils/server/find_plan.go
@@ -0,0 +1,64 @@
+package server
+
+import (
+	"context"
+	"errors"
+
+	"github.com/sacloud/libsacloud/v2/sacloud"
+	"github.com/sacloud/libsacloud/v2/sacloud/search"
+	"github.com/sacloud/libsacloud/v2/sacloud/types"
+)
+
+// PlanFinder .
+type PlanFinder interface {
+	Find(ctx context.Context, zone string, conditions *sacloud.FindCondition) (*sacloud.ServerPlanFindResult, error)
+}
+
+// FindPlanRequest サーバプラン検索パラメータ
+type FindPlanRequest struct {
+	Core       int
+	MemoryGB   int
+	Commitment types.ECommitment
+	Generation types.EPlanGeneration
+}
+
+func (f *FindPlanRequest) findCondition() *sacloud.FindCondition {
+	cond := &sacloud.FindCondition{
+		Sort: search.SortKeys{
+			{Key: "Generation", Order: search.SortDesc},
+		},
+		Filter: search.Filter{
+			search.Key("Commitment"): types.Commitments.Standard,
+		},
+	}
+	if f.Core > 0 {
+		cond.Filter[search.Key("CPU")] = f.Core
+	}
+	if f.MemoryGB > 0 {
+		cond.Filter[search.Key("MemoryMB")] = f.MemoryGB * 1024
+	}
+	if f.Generation != types.PlanGenerations.Default {
+		cond.Filter[search.Key("Generation")] = f.Generation
+	}
+	if f.Commitment != types.Commitments.Unknown && f.Commitment != types.Commitments.Standard {
+		cond.Filter[search.Key("Commitment")] = f.Commitment
+	}
+	return cond
+}
+
+// FindPlan サーバプラン検索
+func FindPlan(ctx context.Context, finder PlanFinder, zone string, param *FindPlanRequest) (*sacloud.ServerPlan, error) {
+	var cond *sacloud.FindCondition
+	if param != nil {
+		cond = param.findCondition()
+	}
+
+	searched, err := finder.Find(ctx, zone, cond)
+	if err != nil {
+		return nil, err
+	}
+	if searched.Count == 0 || len(searched.ServerPlans) == 0 {
+		return nil, errors.New("server plan not found")
+	}
+	return searched.ServerPlans[0], nil
+}
---
+++ utils/server/find_plan_test.go
@@ -0,0 +1,102 @@
+package server
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/sacloud/libsacloud/v2/sacloud"
+	"github.com/stretchr/testify/require"
+)
+
+type dummyPlanFinder struct {
+	plans []*sacloud.ServerPlan
+	err   error
+}
+
+func (f *dummyPlanFinder) Find(ctx context.Context, zone string, conditions *sacloud.FindCondition) (*sacloud.ServerPlanFindResult, error) {
+	if f.err != nil {
+		return nil, f.err
+	}
+
+	return &sacloud.ServerPlanFindResult{
+		Total:       len(f.plans),
+		Count:       len(f.plans),
+		ServerPlans: f.plans,
+	}, nil
+}
+
+func TestFindPlan(t *testing.T) {
+	cases := []struct {
+		msg    string
+		in     *FindPlanRequest
+		finder PlanFinder
+		out    *sacloud.ServerPlan
+		err    error
+	}{
+		{
+			msg: "finder returns unexpected error",
+			in:  nil,
+			finder: &dummyPlanFinder{
+				err: errors.New("dummy"),
+			},
+			out: nil,
+			err: errors.New("dummy"),
+		},
+		{
+			msg: "finder returns multiple result",
+			in: &FindPlanRequest{
+				Core: 1,
+			},
+			finder: &dummyPlanFinder{
+				plans: []*sacloud.ServerPlan{
+					{ID: 1},
+					{ID: 2},
+				},
+			},
+			out: &sacloud.ServerPlan{ID: 1},
+			err: nil,
+		},
+		{
+			msg: "with nil find parameter",
+			in: &FindPlanRequest{
+				Core: 1,
+			},
+			finder: &dummyPlanFinder{
+				plans: []*sacloud.ServerPlan{
+					{ID: 1},
+				},
+			},
+			out: &sacloud.ServerPlan{ID: 1},
+			err: nil,
+		},
+		{
+			msg: "with nil find parameter",
+			in: &FindPlanRequest{
+				Core: 1,
+			},
+			finder: &dummyPlanFinder{
+				plans: []*sacloud.ServerPlan{
+					{ID: 1},
+				},
+			},
+			out: &sacloud.ServerPlan{ID: 1},
+			err: nil,
+		},
+		{
+			msg: "plan not found",
+			in:  nil,
+			finder: &dummyPlanFinder{
+				plans: []*sacloud.ServerPlan{},
+			},
+			out: nil,
+			err: errors.New("server plan not found"),
+		},
+	}
+
+	for _, tc := range cases {
+		res, err := FindPlan(context.Background(), tc.finder, "tk1v", tc.in)
+		require.Equal(t, tc.err, err, tc.msg)
+		require.Equal(t, tc.out, res, tc.msg)
+	}
+}
