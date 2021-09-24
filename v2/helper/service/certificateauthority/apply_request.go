cd73819e0db6f7f680d3d9872445e5d516f85e0b
Kazumichi Yamamoto <yamamoto.febc@gmail.com>
2021-09-24 09:50:03 +0900

    helper/service: Managed PKI

---
+++ v2/helper/service/certificateauthority/apply_request.go
@@ -0,0 +1,66 @@
+// Copyright 2016-2021 The Libsacloud Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package certificateauthority
+
+import (
+	"time"
+
+	"github.com/sacloud/libsacloud/v2/helper/validate"
+	"github.com/sacloud/libsacloud/v2/sacloud"
+	"github.com/sacloud/libsacloud/v2/sacloud/types"
+)
+
+type ApplyRequest struct {
+	ID types.ID `request:"-"`
+
+	Name        string `validate:"required"`
+	Description string `validate:"min=0,max=512"`
+	Tags        types.Tags
+	IconID      types.ID
+
+	Country          string
+	Organization     string
+	OrganizationUnit []string
+	CommonName       string
+	NotAfter         time.Time
+
+	Clients []*ClientCert // Note: API的に証明書の削除はできないため、指定した以上の証明書が存在する可能性がある
+	Servers []*ServerCert // Note: API的に証明書の削除はできないため、指定した以上の証明書が存在する可能性がある
+
+	WaitDuration time.Duration // 証明書発行待ち時間、省略した場合10秒
+}
+
+func (req *ApplyRequest) Validate() error {
+	return validate.Struct(req)
+}
+
+func (req *ApplyRequest) Builder(caller sacloud.APICaller) (*Builder, error) {
+	return &Builder{
+		ID:               req.ID,
+		Name:             req.Name,
+		Description:      req.Description,
+		Tags:             req.Tags,
+		IconID:           req.IconID,
+		Country:          req.Country,
+		Organization:     req.Organization,
+		OrganizationUnit: req.OrganizationUnit,
+		CommonName:       req.CommonName,
+		NotAfter:         req.NotAfter,
+		Clients:          req.Clients,
+		Servers:          req.Servers,
+		WaitDuration:     req.WaitDuration,
+		Client:           sacloud.NewCertificateAuthorityOp(caller),
+	}, nil
+}
---
+++ v2/helper/service/certificateauthority/service_test.go
@@ -0,0 +1,74 @@
+// Copyright 2016-2021 The Libsacloud Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package certificateauthority
+
+import (
+	"testing"
+	"time"
+
+	"github.com/sacloud/libsacloud/v2/sacloud"
+	"github.com/sacloud/libsacloud/v2/sacloud/testutil"
+	"github.com/sacloud/libsacloud/v2/sacloud/types"
+)
+
+func TestCertificateAuthorityService_CRUD(t *testing.T) {
+	if !testutil.IsAccTest() {
+		t.Skip("TestCertificateAuthorityService_CRUD only exec when running an Acceptance Test")
+	}
+
+	svc := New(testutil.SingletonAPICaller())
+	name := testutil.ResourceName("ca")
+
+	testutil.RunCRUD(t, &testutil.CRUDTestCase{
+		Parallel:           true,
+		PreCheck:           nil,
+		SetupAPICallerFunc: testutil.SingletonAPICaller,
+		Setup:              nil,
+		Create: &testutil.CRUDTestFunc{
+			Func: func(ctx *testutil.CRUDTestContext, _ sacloud.APICaller) (interface{}, error) {
+				return svc.Create(&CreateRequest{
+					Name:             name,
+					Description:      "test",
+					Tags:             types.Tags{"tag1", "tag2"},
+					Country:          "JP",
+					Organization:     "usacloud",
+					OrganizationUnit: []string{"ou1","ou2"},
+					CommonName:       "www.usacloud.jp",
+					NotAfter:         time.Now().Add(365 * 24 * time.Hour),
+					Clients:          []*ClientCert{
+						{
+							Country:                   "JP",
+							Organization:              "usacloud",
+							CommonName:                "client.usacloud.jp",
+							NotAfter:                  time.Now().Add(365 * 24 * time.Hour),
+							IssuanceMethod:            "url",
+						},
+					},
+					WaitDuration:     0,
+				})
+			},
+		},
+		Read: &testutil.CRUDTestFunc{
+			Func: func(ctx *testutil.CRUDTestContext, _ sacloud.APICaller) (interface{}, error) {
+				return svc.Read(&ReadRequest{ID: ctx.ID})
+			},
+		},
+		Delete: &testutil.CRUDTestDeleteFunc{
+			Func: func(ctx *testutil.CRUDTestContext, _ sacloud.APICaller) error {
+				return svc.Delete(&DeleteRequest{ID: ctx.ID})
+			},
+		},
+	})
+}
